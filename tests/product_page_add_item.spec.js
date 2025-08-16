9be82122de0f6f0ce14ae11e8e2f87d82bc09fc9
lucasbarrosqaanalyst <lucasbarrosprofessional@gmail.com>
2025-08-16 20:24:55 -0300

    Verify Shopping Cart items and define baseUrl

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -1,13 +1,17 @@
 import { test, expect } from "@playwright/test";
 
 test("Product page added to basket", async({page})=>{
-    await page.goto("https://automationteststore.com/");
+    //baseUrl to go to page https://automationteststore.com/
+    await page.goto("/");
     
-    const addToBasketButton = page.locator('[title="Add to Cart"]').first();
-    // const basketCounter = page.locator('[class="label label-orange font14"]').first();
-
+    const getItemToSelect = page.locator('[class="thumbnail"]').first();
+    const addToCartButton = page.getByRole('link', { name: 'Add to Cart' }).first();
+    const checkShoppingCart = page.getByRole('heading', { name: 'Shopping Cart' }).first();
     
-    await addToBasketButton.click();
-    // await expect(basketCounter).toHaveText("1");
+    
+    await getItemToSelect.click();
+    await addToCartButton.click();
+    await page.waitForURL("/index.php?rt=checkout/cart");
+    await expect(checkShoppingCart).toHaveText('Shopping Cart');
     
 });
